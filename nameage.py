c862e03d67e706f376af0730b2db97e46796e637
WLopesF <williamlopesfreitas789@gmail.com>
2024-10-10 10:59:51 -0300

    Add files via upload

---
+++ nameage.py
@@ -0,0 +1,8 @@
+string1 = input("Digite o seu primeiro nome")
+string2 = input("Digite o seu sobrenome")
+string3 = int(input("Digite a sua idade"))
+
+print("Olá, o seu nome é", string1, "seu sobrenome é", string2, "e você tem", string3, "anos de idade.")
+
+
+ 
