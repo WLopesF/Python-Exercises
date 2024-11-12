6094416b68386e01867a30f4f41b66fe3df85f3f
WLopesF <williamlopesfreitas789@gmail.com>
2024-11-12 18:30:24 -0300

    Update nameage.py

--- nameage.py
+++ nameage.py
@@ -4,11 +4,10 @@ string3 = int(input("Digite a sua idade: "))
 string4 = input("Você é estudante?")
                 
 if string4 in["Sim", "Sou"]:
-   string5 = input("Em qual ano ou faculdade e universidade?")
-   print("Olá, o seu nome é", string1, "seu sobrenome é", string2, "e você tem", string3, "anos de idade. Você é estudante do/de na", string, "."" 
+   string5 = input("Em qual ano ou área?")
+if (string5):
+   string6 = input("Em qual escola ou universidade?")
+   print("Olá, o seu nome é", string1, "seu sobrenome é", string2, "e você tem", string3, "anos de idade. Você é estudante do", string5, "na", string6, ".")
 else:
-   print("Olá, o seu nome é", string1, "seu sobrenome é", string2, "e você tem", string3, "anos de idade e você não é estudante. Nunca é tarde demais pra aprender!"
+   print("Olá, o seu nome é", string1, "seu sobrenome é", string2, "e você tem", string3, "anos de idade e você não é estudante. Nunca é tarde demais pra aprender!")
          
-   
-
- 
